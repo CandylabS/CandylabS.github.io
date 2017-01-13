bdd0f1a70a6b9d74aa89ae8b00affc6f18940aed
Alice Chen <alice911003@gmail.com>
2017-01-12 22:21:05 -0500

    popups

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+$('.video').magnificPopup({
+  type: 'iframe',  
+  iframe: {
+     markup: '<div class="mfp-iframe-scaler">'+
+                '<div class="mfp-close"></div>'+
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+              '</div>'
+  }
+});
